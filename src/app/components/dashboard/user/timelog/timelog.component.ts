a99d20e2813b4bebe6310eac100cf6dc5487eb41
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2021-07-07 14:48:17 +0545

    Weekly calender added using momentjs

--- src/app/components/dashboard/user/timelog/timelog.component.ts
+++ src/app/components/dashboard/user/timelog/timelog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { AddTimelogComponent } from './addTimelog/addTimelog.component';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-timelog',
@@ -10,12 +11,36 @@ import { AddTimelogComponent } from './addTimelog/addTimelog.component';
 export class TimelogComponent implements OnInit {
 
   bsModalRef: BsModalRef;
+  todayDate;
+  weekDays = [];
+  currentMonth: string;
+  currentYear: string;
+
   constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
+    this.getCurrentWeek();
   }
 
   openModalWithComponent() {
     this.bsModalRef = this.modalService.show(AddTimelogComponent, {});
+  }
+
+  getCurrentWeek() {
+    let currentDate = moment();
+    let weekStart = currentDate.clone().startOf('week');
+
+    this.todayDate = currentDate.format().split("T")[0];
+    this.currentMonth = currentDate.format("MMMM");
+    this.currentYear = currentDate.format("YYYY");
+
+    for (let i = 0; i <= 6; i++) {
+      this.weekDays.push(moment(weekStart).add(i, 'days').format().split("T")[0]);
+    }
+
+  }
+
+  onDateClick(weekDay) {
+    console.log(weekDay);
   }
 }
